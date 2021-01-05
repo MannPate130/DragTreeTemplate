d4d88eab282d5a5cd50a10b348cac0b31363dc88
Mann Name <mannhp1103@outlook.com>
2021-01-05 12:57:58 -0500

    Update

--- DragTree/Form1.cs
+++ DragTree/Form1.cs
@@ -36,27 +36,62 @@ namespace DragTree
         private void startButton_Click(object sender, EventArgs e)
         {
             // TODO - start the timer
-
+            lightTimer.Start();
         }
 
         private void goButton_Click(object sender, EventArgs e)
         {
             // TODO - stop the stopwatch
-
+            stopWatch.Stop();
             // TODO - check if the ellapsed time in milliseconds is > 0. 
             // If yes show the time.
             // If no show "FOUL START" 
-
+            if (lightTimer.Enabled == false)
+            {
+                lightTimer.Enabled = true;
+                stopWatch.Start();
+            }
+            else
+            {
+                lightTimer.Enabled = false;
+                stopWatch.Stop();
+                timeLabel.Text = $"{stopWatch.Elapsed.ToString(@"ss\:ff")}";
+            }
+            if (stopWatch.ElapsedMilliseconds > 0)
+            {
+                timeLabel.Text = $"{stopWatch.Elapsed.ToString(@"ss\:ff")}";
+            }
+            else 
+            {
+                timeLabel.Text = "FOUL START";
+            }
         }
 
         private void resetButton_Click(object sender, EventArgs e)
         {
             // TODO - reset the stopwatch
+            stopWatch.Reset();
 
             // TODO - put rows 1-3 colours back to DimGray and row 4 back to DarkOliveGreen
-
+            row1col1.BackColor = Color.DimGray;
+            row1col2.BackColor = Color.DimGray;
+            row2col1.BackColor = Color.DimGray;
+            row2col2.BackColor = Color.DimGray;
+            row3col1.BackColor = Color.DimGray;
+            row3col2.BackColor = Color.DimGray;
+            row4col1.BackColor = Color.DarkOliveGreen;
+            row4col2.BackColor = Color.DarkOliveGreen;
             // TODO - reset row value and timeLabel text
-
+            timeLabel.Text = "";
+            row1col1.Text = "";
+            row1col2.Text = "";
+            row2col1.Text = "";
+            row2col2.Text = "";
+            row3col1.Text = "";
+            row3col2.Text = "";
+            row4col1.Text = "";
+            row4col2.Text = "";
+            currentRow = 0;
         }
 
         private void lightTimer_Tick(object sender, EventArgs e)
@@ -74,6 +109,10 @@ namespace DragTree
                 case 3:
                     row3col1.BackColor = Color.Yellow;
                     row3col2.BackColor = Color.Yellow;
+                    break;
+                case 4:
+                    row4col1.BackColor = Color.LightGreen;
+                    row4col2.BackColor = Color.LightGreen;
                     break;
             }
                
